4e3a6334f4177c14a04e27ac1508cd3d91cc610a
rubaiyat07 <rubaiyat97wd@gmail.com>
2025-08-05 16:07:01 +0600

    Added logo

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -8,12 +8,9 @@ const NavbarComponent = () => {
     <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container">
         <Link to="/" className="navbar-brand d-flex align-items-center">
-          <span className="logo-icon px-3 py-2 rounded me-2 d-inline-flex align-items-center justify-content-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
-              <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z"/>
-            </svg>
+          <span className="logo-icon rounded me-2 d-inline-flex align-items-center justify-content-center">
+            <img src="/assets/img/logo.png" alt="Logo" className="img-fluid" />
           </span>
-          <span className="fw-bold">E-Tender Portal</span>
         </Link>
         
         <button
